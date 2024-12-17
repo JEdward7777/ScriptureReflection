28a7d36ef15657c09e5baea9a1355af447109db5
Joshua Lansford <Joshua@Lansfords.com>
2024-12-17 14:53:05 -0500

    Added some verse parsing so we can have some compact selection output info

---
+++ test_verse_parsing.py
@@ -0,0 +1,86 @@
+from verse_parsing import to_range
+
+
+refs = [
+    "GEN 1:1",
+    "GEN 1:2",
+    "GEN 1:3",
+    "GEN 1:4",
+    "GEN 2:1",
+    "GEN 2:2",
+    "GEN 2:3",
+    "GEN 3:1",
+    "GEN 3:2",
+    "GEN 3:3",
+    "EXD 1:1",
+    "EXD 1:2",
+    "EXD 1:3",
+    "EXD 2:1",
+    "EXD 2:2",
+    "EXD 2:3",
+]
+def test_single():
+    assert to_range(["GEN 1:1"], refs) == "GEN 1:1"
+def test_single_range():
+    assert to_range(["GEN 1:1","GEN 1:2", "GEN 1:3"], refs ) == "GEN 1:1-3"
+def test_chapter_range():
+    assert to_range( ["GEN 1:1",
+    "GEN 1:2",
+    "GEN 1:3",
+    "GEN 1:4",
+    "GEN 2:1",
+    "GEN 2:2",
+    "GEN 2:3"], refs ) == "GEN 1-2"
+def test_verse_list():
+    assert to_range(["GEN 1:1", "GEN 1:3"], refs) == "GEN 1:1,3"
+def test_chapter_list():
+    assert to_range( [
+    "GEN 1:1",
+    "GEN 1:2",
+    "GEN 1:3",
+    "GEN 1:4",
+    "GEN 3:1",
+    "GEN 3:2",
+    "GEN 3:3" ], refs ) == "GEN 1,3"
+
+def test_whole_book():
+    assert to_range( [
+    "GEN 1:1",
+    "GEN 1:2",
+    "GEN 1:3",
+    "GEN 1:4",
+    "GEN 2:1",
+    "GEN 2:2",
+    "GEN 2:3",
+    "GEN 3:1",
+    "GEN 3:2",
+    "GEN 3:3", ], refs ) == "GEN"
+
+def test_book_range():
+    test_refs = [
+        "GEN 1:1",
+        "GEN 1:2",
+        "GEN 1:3",
+        "GEN 1:4",
+        "GEN 2:1",
+        "GEN 2:2",
+        "GEN 2:3",
+        "GEN 3:1",
+        "GEN 3:2",
+        "GEN 3:3",
+        "EXD 1:1",
+        "EXD 1:2",
+        "EXD 1:3",
+        "EXD 2:1",
+        "EXD 2:2",
+        "EXD 2:3",
+    ]
+    assert to_range( test_refs, refs ) == "GEN-EXD"
+
+def test_range_and_list():
+    test_refs = [
+        "GEN 1:1",
+        "GEN 1:3",
+        "GEN 1:4",
+    ]
+    assert to_range( test_refs, refs ) == "GEN 1:1,3-4"
